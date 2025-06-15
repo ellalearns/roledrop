5522c9c64ee98c2573a00dba7ce0501915e5cace
ellalearns <ellalearnscopy@gmail.com>
2025-06-15 09:31:09 +0100

    implement sqllite3

---
+++ db/db.py
@@ -0,0 +1,101 @@
+import sqlite3
+import datetime
+
+
+def init_db():
+    """
+    """
+    conn = sqlite3.connect("rd_users.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+                   id INTEGER PRIMARY KEY,
+                   status TEXT,
+                   date_joined TEXT,
+                   date_subscribed TEXT,
+                   sites TEXT,
+                   categories TEXT
+                   )
+    """)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS payments (
+                   id INTEGER PRIMARY KEY,
+                   reference TEXT
+                   )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def add_user_to_db(user_id):
+
+    date_joined = datetime.datetime.now().isoformat()
+    sites = "linkedin,"
+    categories = "all,"
+
+    conn = sqlite3.connect("rd_users.dp")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT OR IGNORE INTO users (
+                   id,
+                   status,
+                   date_joined,
+                   date_subscribed,
+                   sites,
+                   categories
+                   )
+    """, (
+        user_id,
+        "trial",
+        date_joined,
+        date_joined,
+        sites,
+        categories,
+    ))
+
+    conn.commit()
+    conn.close()
+
+
+def get_user_info(user_id):
+    """
+    """
+    conn = sqlite3.connect("rd_users.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
+
+    user = cursor.fetchone()
+
+    conn.close()
+
+    if user:
+        return {
+            "id": user[0],
+            "status": user[1],
+            "date_joined": user[2],
+            "date_subscribed": user[3],
+            "sites": user[4],
+            "categories": user[5]
+        }
+    else:
+        return None
+
+
+def check_user(user_id):
+    conn = sqlite3.connect("rd_users.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT * FROM users WHERE id = ?
+    """, (user_id,))
+    user = cursor.fetchone()
+    conn.close()
+
+    if user:
+        return user[0]
+    
+    return None
