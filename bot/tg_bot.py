94adbde2503b3fe74773d6f3ced3e74e93ab6dbf
ellalearns <ellalearnscopy@gmail.com>
2025-06-15 10:37:01 +0100

    add inline keyboards

--- bot/tg_bot.py
+++ bot/tg_bot.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes, CommandHandler
 import logging
 from server.server import application
@@ -11,6 +11,27 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
+
+keyboard = InlineKeyboardMarkup([
+    [InlineKeyboardButton("Info", callback_data="info")],
+    [InlineKeyboardButton("Edit", callback_data="edit")],
+    [InlineKeyboardButton("Help", callback_data="help")],
+    [InlineKeyboardButton("Pay", callback_data="pay")]
+])
+
+category_keyboard = InlineKeyboardMarkup([
+    [InlineKeyboardButton("content writing", callback_data="toggle_writing")],
+    [InlineKeyboardButton("software development", callback_data="toggle_dev")],
+    [InlineKeyboardButton("virtual assistant", callback_data="toggle_assitant")],
+    [InlineKeyboardButton("customer service", callback_data="toggle_service")],
+    [InlineKeyboardButton("data entry", callback_data="toggle_data")],
+    [InlineKeyboardButton("design", callback_data="toggle_design")],
+    [InlineKeyboardButton("ai/ml engineering", callback_data="toggle_ml")],
+    [InlineKeyboardButton("social media management", callback_data="toggle_social")],
+    [InlineKeyboardButton("project management", callback_data="toggle_project")],
+    [InlineKeyboardButton("others", callback_data="toggle_others")],
+    [InlineKeyboardButton("Save", callback_data="save")],
+])
 
 async def start(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     """
